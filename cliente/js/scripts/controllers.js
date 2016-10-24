5198fa02b5ea089f4e6a01457e41fa48ecd07428
Carlos Pitre <carlsjulio@gmail.com>
2016-10-24 11:21:11 -0500

    mapa

--- cliente/js/scripts/controllers.js
+++ cliente/js/scripts/controllers.js
@@ -159,6 +159,16 @@ app.controller('ProfesionalCtrl', function ($scope,profesionalService,clienteSer
                       infoWindow.open(map, marker);
                   });
               })(marker, label);
+            var cityCircle = new google.maps.Circle({
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+              map: map,
+              center: {lat: position.coords.latitude, lng:position.coords.longitude},
+              radius: Math.sqrt(100) * 100
+            });
 
 
             for (var i = 0; i < $scope.Profesionales.length; i++) {
